453c0dd0d2f90f615c1a3ba8bdf9c86e19c74eb0
hustcc <i@hust.cc>
2018-06-20 13:21:39 +0800

    save bytes

--- src/constant.js
+++ src/constant.js
@@ -5,6 +5,6 @@
 
 export const SizeSensorId = 'size-sensor-id';
 
-export const SensorStyle = 'display: block; position: absolute; top: 0; left: 0; height: 100%; width: 100%; overflow: hidden; pointer-events: none; z-index: -1; opacity: 0;';
+export const SensorStyle = 'display:block;position:absolute;top:0;left:0;height:100%;width:100%;overflow:hidden;pointer-events:none;z-index:-1;opacity:0';
 
 export const SensorClassName = 'size-sensor-object';
