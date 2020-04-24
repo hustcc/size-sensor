c7c019367b7abf62e3c40e4700d8337f9e60337f
Thomas Zimmermann <thomas.zimmermann@signavio.com>
2020-04-24 11:55:30 +0200

    fix: explicit style setting for CSP conformity

--- src/constant.js
+++ src/constant.js
@@ -5,8 +5,6 @@
 
 export const SizeSensorId = 'size-sensor-id';
 
-export const SensorStyle = 'display:block;position:absolute;top:0;left:0;height:100%;width:auto;overflow:hidden;pointer-events:none;z-index:-1;opacity:0';
-
 export const SensorClassName = 'size-sensor-object';
 
 export const SensorTabIndex = '-1';
