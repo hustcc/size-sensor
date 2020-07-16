8ae101bd204444142b239410240df475344794b5
hustcc <i@hust.cc>
2020-07-16 13:39:08 +0800

    test: add test case for #16

---
+++ __tests__/unbind.spec.js
@@ -0,0 +1,26 @@
+import { clear, bind } from '../src';
+
+describe('unbind', () => {
+  // 创建 div
+  const div = document.createElement('div');
+  document.body.appendChild(div);
+  div.innerHTML = `<div id="wrapper"></div>`;
+
+  const wrapper = document.getElementById('wrapper');
+
+  test('clear', () => {
+    bind(wrapper, () => {});
+    expect(wrapper.getAttribute('size-sensor-id')).not.toBeNull();
+
+    clear(wrapper);
+    expect(wrapper.getAttribute('size-sensor-id')).toBeNull();
+  });
+
+  test('unbind', () => {
+    const unbind = bind(wrapper, () => {});
+    expect(wrapper.getAttribute('size-sensor-id')).not.toBeNull();
+
+    unbind();
+    expect(wrapper.getAttribute('size-sensor-id')).not.toBeNull();
+  });
+});
